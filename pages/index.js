7da9d12c88ba9c6e32a45f3948d500104b1069ce
Rafael Roriz Barbosa <rafaelbarbosa525@gmail.com>
2023-09-24 14:27:54 +0000

    Implementar página "Em construção"

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-  return <h1>Welcome to the home</h1>;
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1>Em construção...</h1>
+    </div>
+  );
 }
 
 export default Home;
